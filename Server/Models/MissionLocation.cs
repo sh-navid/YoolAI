0788a55ac478b87ff6c459ad614f05ba7826b07a
sh-navid <shamshirsaz.navid@live.com>
2025-10-10 20:42:31 +0330

    [Feature] Added Deadline field to MissionLocation model

--- Server/Models/MissionLocation.cs
+++ Server/Models/MissionLocation.cs
@@ -23,6 +23,7 @@ namespace Server.Models
         
         public bool IsDone { get; set; }
 
+        public DateTime Deadline { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     }
